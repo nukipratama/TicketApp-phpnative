5058b29a75cbfc1cc4f67339adb1d0370ea6ad74
Nuki Pratama <nukipratamaa@gmail.com>
2019-06-21 09:52:00 +0700

    volunteer ok

---
+++ volunteer.telkomuniversityrun.com/page_success.php
@@ -0,0 +1,3 @@
+<meta http-equiv="refresh" content="5;url=https://volunteer.telkomuniversityrun.com">
+<?php
+echo "Volunteer Registration Success, Thank You.<br>We will contact you by email addreess that you've provided.<br>Redirecting to Home..";
--- volunteer/page_form.php
+++
@@ -1,2 +0,0 @@
-<?php
-include '../page_header.php';
